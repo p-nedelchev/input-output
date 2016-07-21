21c6cb4f51a9ba7fa421e7a2f87f2eb9f1241d68
Petar Nedelchev <peter.krasimirov@gmail.com>
2016-07-21 13:09:32 +0300

    Second task from Input-Output

---
+++ src/consoletofile/Demo.java
@@ -0,0 +1,13 @@
+package consoletofile;
+
+/**
+ * @author Petar Nedelchev <peter.krasimirov@gmail.com>
+ */
+public class Demo {
+
+    public static void main(String[] args) {
+        ReaderToFile reader = new ReaderToFile();
+        reader.readToFile();
+    }
+
+}
